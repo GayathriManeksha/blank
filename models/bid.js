d4e319d8e325b79144a68ea04d6d1d3391e0d9ee
GayathriManeksha <gaya3maneksha@github.com>
2024-04-17 22:02:56 +0530

    Bid modified

--- models/bid.js
+++ models/bid.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bidSchema = new mongoose.Schema({
     amount: { type: Number }, // For bid messages
     timestamp: { type: String, default: Date.now },
-    chatId: { type: mongoose.Schema.Types.ObjectId, ref: 'Chat', required: true }, // Reference to the user making the request
+    // chatId: { type: mongoose.Schema.Types.ObjectId, ref: 'Chat', required: true }, // Reference to the user making the request
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Reference to the user making the request
     workerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Worker' }, // Reference to the assigned worker
     approval: { type: Number, default: 0 },
